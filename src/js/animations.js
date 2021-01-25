864681f76ed31e7fe14f099881acd9b90f06131e
feinoko <feinoko@gmail.com>
2021-01-25 19:40:25 +0100

    attempt fade

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,17 +1,18 @@
-const elements = document.querySelectorAll('*');
+// const elements = document.querySelectorAll('*');
 
-window.onload = function fadeAll() {
-  elements.forEach(function(element, index) {
+// window.onload = function fadeAll() {
+//   elements.forEach(function(element, index) {
     
-    setTimeout(function () {
-      console.log(element);
-      // for (let i = 0; i<100 ;i++) {
-      //   element.style.opacity = 0;
-      //   setTimeout(function () {
-      //     element.style.opacity += 0.01;
-      //   }, 50);
-      // }
+//     setTimeout(function () {
+//       console.log(element);
+//       // for (let i = 0; i<100 ;i++) {
+//       //   element.style.opacity = 0;
+//       //   setTimeout(function () {
+//       //     element.style.opacity += 0.01;
+//       //   }, 50);
+//       // }
       
-    }, 100 * index);
-  })
-}
+//     }, 100 * index);
+//   })
+// }
+
