c84d905b9446fc952aba3556ab6a8dbb96a4abb0
feinoko <feinoko@gmail.com>
2021-02-01 06:56:24 +0100

    text remove : update
    removing text from quote seems working

--- src/js/textInteraction.js
+++ src/js/textInteraction.js
@@ -21,6 +21,7 @@ Steps :
 ========================*/
 
 // to limit the # of calls on mouse over (performance save)
+import { indexOf } from 'lodash';
 import { throttle } from 'throttle-debounce';
 // see https://www.npmjs.com/package/throttle-debounce
 // finally was not used, keeping in for the record, to leave a trace that this exists (or you can use lodash too)
@@ -38,6 +39,26 @@ class textInteraction {
     document.body.addEventListener('mouseover', function(e) {
       
       console.log(`hovering body part: ${e.target}`);
+      const ELquoteText = document.getElementById('quote-text');
+      if (e.target === ELquoteText) {
+        let quoteText = ELquoteText.textContent;
+        // wait a bit before start removing
+        setTimeout(() => {
+          // loop that removes the last char at each iteration, with a set delay
+          setInterval(() => {
+            // exit loop when string is empty
+            if(quoteText.length===0) {return;};
+            // remove the last char
+            quoteText = quoteText.substring(0,quoteText.length-1);
+            console.log(quoteText.length);
+            // reassign the string without the last char
+            ELquoteText.textContent = quoteText;
+          },20);
+        }, 1300);
+        
+        
+        
+      }
       
       
     })
